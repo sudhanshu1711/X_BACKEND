1baf333683df750f9b6bf5805f8bf72e2d47e98a
sudhanshu1711 <sudhanshu2679@gmail.com>
2025-07-14 10:28:40 +0530

    made changes in the comment model

--- src/models/comment.js
+++ src/models/comment.js
@@ -5,9 +5,21 @@ const commentSchema = new mongoose.Schema({
         type:String,
         required:true
     },
-    userEmail:{
-        type:String
-    }
+   userId:{
+    type:mongoose.Schema.Types.ObjectId,
+    ref:'User',
+    required:true
+   },
+   onModel:{
+    type:String,
+    required:true,
+    enum:['Tweet','Comment']
+   },
+   commentable:{
+    type:mongoose.Schema.Types.ObjectId,
+    ref:'onModel',
+    required:true
+   }
 },{timestamps:true})
 
 const Comment = mongoose.model('Comment',commentSchema)
